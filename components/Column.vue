47cff751d6479b7f298978d124fd61ebbf5ccf47
Sven Aarts <sven.aarts@telenetgroup.be>
2018-08-27 16:24:31 +0200

    add a support for a column header & footer
    to create call to actions on our blockchain page

--- components/Column.vue
+++ components/Column.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="Column" v-editable="blok">
-    <component :key="blok._uid" v-for="blok in blok.blocks" :blok="blok" :is="blok.component"></component>
+    <div class="Column-header">
+      <component :key="blok._uid" v-for="blok in blok.columnheader" :blok="blok" :is="blok.component"></component>
+    </div>
+    <div class="Column-content">
+      <component :key="blok._uid" v-for="blok in blok.blocks" :blok="blok" :is="blok.component"></component>
+    </div>
+    <div class="Column-footer">
+      <component :key="blok._uid" v-for="blok in blok.columnfooter" :blok="blok" :is="blok.component"></component>
+    </div>
   </div>
 </template>
 
@@ -13,5 +21,11 @@
 <style lang="scss">
   .Column {
     padding: 0 $spacer/2;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .Column-footer {
+    margin-top: auto;
   }
 </style>

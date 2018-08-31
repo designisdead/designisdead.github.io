68fd5a5a749933c5ffe4e1a7ddc758f681285bb1
Sven Aarts <sven.aarts@telenetgroup.be>
2018-08-31 08:05:23 +0200

    [DIDW-28] Support external links

--- components/Button.vue
+++ components/Button.vue
@@ -1,15 +1,37 @@
 <template>
-  <nuxt-link v-editable="blok"
-    class="Button"
-    :class="[blok.style]"
-    :key="blok.id"
-    :to="blok.link.url">
-    {{ blok.text }}
-  </nuxt-link>
+  <div>
+    <nuxt-link v-editable="blok"
+               v-if="!externalLink"
+               class="Button"
+               :class="[blok.style]"
+               :key="blok.id"
+               :to="blok.link.cached_url">
+      {{ blok.text }}
+    </nuxt-link>
+    <a class="Button" :href="blok.link.cached_url"
+      rel="noopener"
+      :class="[blok.style]"
+      v-if="externalLink">
+      {{ blok.text }}
+    </a>
+  </div>
 </template>
 
 <script>
   export default {
-    props: ['blok']
+    props: ['blok'],
+    computed: {
+      externalLink() {
+        /*
+          ^ - Only match at the beginning of the string
+          http - Match the literal string "http"
+          s? - Optionally match an "s"
+          : - Match a colon
+          \/\/ - Escape the "/" characters since they mark the beginning/end of the regular expression
+          The "i" after the regular expression makes it case-insensitive so it will match "HTTP://", etc.
+         */
+         return /^https?:\/\//i.test(this.blok.link.cached_url);
+      }
+    }
   }
 </script>

63313bdc2ef6ae17733754d90e2e9e50a15ad76a
Unknown <info@svenaarts.be>
2018-10-15 12:08:59 +0200

    smaller font on mobile

--- components/TextRotator.vue
+++ components/TextRotator.vue
@@ -12,98 +12,99 @@
 </template>
 
 <script>
-  export default {
-    props: {
-      blok: {
-        type: Object,
-        default: function () {
-          return {}
-        }
+export default {
+  props: {
+    blok: {
+      type: Object,
+      default: function() {
+        return {};
       }
     }
   }
+};
 </script>
 
 <style lang="scss">
-  .TextRotator {
-    font-family: 'quick_toyregular';
-    margin: 0 auto;
-    font-size: 100px;
-    line-height: 0.8;
-    color: white;
-    margin-top: 15px;
-    width: 300px;
-    height: 160px;
-    position: relative;
-    transform: translateX(-20px);
-    zoom: 1.1;
-    @media only screen and (max-width: 480px) {
-      zoom: 0.7;
+.TextRotator {
+  font-family: "quick_toyregular";
+  margin: 0 auto;
+  font-size: 100px;
+  line-height: 0.8;
+  color: white;
+  margin-top: 15px;
+  width: 300px;
+  height: 160px;
+  position: relative;
+  transform: translateX(-20px);
+  zoom: 1.1;
+  @media only screen and (max-width: 480px) {
+    zoom: 0.7;
+    font-size: 80px;
+  }
+}
+
+.TextRotator-slide {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  text-align: center;
+  transform: rotateY(-90deg);
+  will-change: transform, opacity;
+  -webkit-animation: textrotator-visible 20s linear infinite;
+  animation: textrotator-visible 20s linear infinite;
+  @for $i from 1 through 10 {
+    &:nth-child(#{$i}) {
+      -webkit-animation-delay: (#{$i * 5s - 5s});
+      animation-delay: (#{$i * 5s - 5s});
     }
   }
+}
 
-  .TextRotator-slide {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    text-align: center;
+@-webkit-keyframes textrotator-visible {
+  0% {
     transform: rotateY(-90deg);
-    will-change: transform, opacity;
-    -webkit-animation: textrotator-visible 20s linear infinite;
-    animation: textrotator-visible 20s linear infinite;
-    @for $i from 1 through 10 {
-      &:nth-child(#{$i}) {
-        -webkit-animation-delay: (#{$i*5s - 5s});
-        animation-delay: (#{$i*5s - 5s});
-      }
-    }
+    opacity: 0;
   }
+  1% {
+    transform: rotateY(0deg);
+    opacity: 1;
+  }
+  24% {
+    transform: rotateY(0deg);
+    opacity: 1;
+  }
+  25% {
+    transform: rotateY(-90deg);
+    opacity: 0;
+  }
+  100% {
+    transform: rotateY(-90deg);
+    opacity: 0;
+  }
+}
 
-  @-webkit-keyframes textrotator-visible {
-    0% {
-      transform: rotateY(-90deg);
-      opacity: 0;
-    }
-    1% {
-      transform: rotateY(0deg);
-      opacity: 1;
-    }
-    24% {
-      transform: rotateY(0deg);
-      opacity: 1;
-    }
-    25% {
-      transform: rotateY(-90deg);
-      opacity: 0;
-    }
-    100% {
-      transform: rotateY(-90deg);
-      opacity: 0;
-    }
+@keyframes textrotator-visible {
+  0% {
+    transform: rotateY(-90deg);
+    opacity: 0;
   }
-
-  @keyframes textrotator-visible {
-    0% {
-      transform: rotateY(-90deg);
-      opacity: 0;
-    }
-    1% {
-      transform: rotateY(0deg);
-      opacity: 1;
-    }
-    24% {
-      transform: rotateY(0deg);
-      opacity: 1;
-    }
-    25% {
-      transform: rotateY(-90deg);
-      opacity: 0;
-    }
-    100% {
-      transform: rotateY(-90deg);
-      opacity: 0;
-    }
+  1% {
+    transform: rotateY(0deg);
+    opacity: 1;
   }
+  24% {
+    transform: rotateY(0deg);
+    opacity: 1;
+  }
+  25% {
+    transform: rotateY(-90deg);
+    opacity: 0;
+  }
+  100% {
+    transform: rotateY(-90deg);
+    opacity: 0;
+  }
+}
 </style>

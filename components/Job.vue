a904b923027932e62bc4f4f50f95b22bc7acb8b1
Unknown <info@svenaarts.be>
2019-01-03 11:04:46 +0100

    fix layout

--- components/Job.vue
+++ components/Job.vue
@@ -5,14 +5,14 @@ This component is used to render the Post content type on Storyblok
 <template>
   <main>
     <article>
-      <header class="Wrapper halfscreen u-textAlignCenter u-backgroundColor--black u-color--light">
+      <header class="Job-header Wrapper halfscreen u-textAlignCenter u-backgroundColor--black u-color--light">
         <wrapper size="large">
           <h1>{{ pagetitle }}</h1>
           <h2 class="small">{{ blok.metadescription }}</h2>
         </wrapper>
       </header>
 
-      <spacer size="huge" />
+      <spacer size="huge"/>
 
       <div
         v-editable="blok"
@@ -25,39 +25,48 @@ This component is used to render the Post content type on Storyblok
       </div>
 
       <footer>
-        <spacer size="medium" />
+        <spacer size="medium"/>
         <wrapper size="medium">
-          <nuxt-link :to="'/jobs'">View all jobs</nuxt-link>
+          <div
+            class="Buttons"
+            style="justify-content: center;">
+            <nuxt-link
+              :to="'/jobs'"
+              class="Button">
+              View all jobs
+            </nuxt-link>
+          </div>
         </wrapper>
       </footer>
 
       <spacer size="small"/>
-      
+
       <doormat/>
     </article>
   </main>
 </template>
 
 <script>
-import moment from "moment";
-export default {
-  props: {
-    blok: {
-      type: Object,
-      default: function() {
-        return {};
+  import moment from "moment";
+
+  export default {
+    props: {
+      blok: {
+        type: Object,
+        default: function () {
+          return {};
+        }
+      },
+      pagetitle: {
+        type: String,
+        default: "Design is Dead Jobs"
       }
-    },
-    pagetitle: {
-      type: String,
-      default: "Design is Dead Jobs"
     }
-  }
-};
+  };
 </script>
 
 <style lang="scss">
-.Post-primaryImage {
-  padding-bottom: $spacer;
-}
+  .Job-header {
+    padding-bottom: $spacer;
+  }
 </style>

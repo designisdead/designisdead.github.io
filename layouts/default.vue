e03cf3578047950dd592cd1975c4d14027ca7f9a
sumerion <kris@sumerion.com>
2019-04-29 17:16:06 +0200

    DIDW-56 use single quotes

--- layouts/default.vue
+++ layouts/default.vue
@@ -338,12 +338,12 @@
           (e.pgo.q = e.pgo.q || []).push(arguments)
         };
         e.pgo.l = (new Date).getTime();
-        r = t.createElement("script");
+        r = t.createElement('script');
         r.src = o;
         r.async = true;
-        i = t.getElementsByTagName("script")[0];
+        i = t.getElementsByTagName('script')[0];
         i.parentNode.insertBefore(r, i)
-      })(window, document, "https://prism.app-us1.com/prism.js", "pgo");
+      })(window, document, 'https://prism.app-us1.com/prism.js', 'pgo');
 
       pgo('setAccount', '223882243');
       pgo('setTrackByDefault', true);

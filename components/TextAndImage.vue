866bc60f38ffde91ae7a25a9c76de4fdd9a81ff9
Amondt <mondt.alexandre@gmail.com>
2019-08-02 13:06:21 +0200

    first commit

---
+++ components/TextAndImage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-editable="blok">
+    <h1>This is my super text and image component</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object, default: function () {
+        return {}
+      }
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
--- plugins/components.js
+++ plugins/components.js
@@ -33,6 +33,7 @@ import EqualColumns from "@/components/EqualColumns.vue";
 import YoutubeEmbed from "@/components/YoutubeEmbed.vue";
 import Mailchimp from '@/components/Mailchimp.vue';
 import Timeline from '@/components/Timeline.vue';
+import TextAndImage from '@/components/TextAndImage.vue';
 
 Vue.component("author", Author);
 Vue.component("icon", Icon);
@@ -68,3 +69,4 @@ Vue.component("equalcolumns", EqualColumns);
 Vue.component("youtubeEmbed", YoutubeEmbed);
 Vue.component("mailchimp", Mailchimp);
 Vue.component("timeline", Timeline);
+Vue.component("textandimage", TextAndImage);

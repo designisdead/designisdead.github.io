173d9731a820aa3bbbff94026dc4d7c111128829
Amondt <mondt.alexandre@gmail.com>
2019-08-19 12:37:28 +0200

    last changes'

---
+++ components/Contact.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 style="text-align: center;">This is my super contact page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

566e010240900512255348634d7b4967bd693e26
Amondt <mondt.alexandre@gmail.com>
2019-08-21 15:51:28 +0200

    revert settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ module.exports = {
   mode: 'universal',
   modules: [
     ['storyblok-nuxt', {
-      // accessToken: 'O2r6aDSsF6m26lYt5NNMzQtt',
-      accessToken: process.env.NODE_ENV === 'production' ? 'O2r6aDSsF6m26lYt5NNMzQtt' : 'AJwMQue3YmvF9GhvSrecTQtt',
+      accessToken: 'AJwMQue3YmvF9GhvSrecTQtt',
+      // accessToken: process.env.NODE_ENV === 'production' ? 'O2r6aDSsF6m26lYt5NNMzQtt' : 'AJwMQue3YmvF9GhvSrecTQtt',
       cacheProvider: 'memory',
       excludeHeaderScript: false
       // excludeHeaderScript: process.env.NODE_ENV == 'production'

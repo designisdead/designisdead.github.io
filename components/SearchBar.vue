b0a2aea4e30589dce53f56c92fb670fa342628a3
Amondt <mondt.alexandre@gmail.com>
2019-09-04 13:58:36 +0200

    fix checkmark tag filter all devices

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -348,8 +348,9 @@ export default {
   .search-bar__tag-list-item__checkbox:checked:after {
     content: url(/checkmark-icon.svg);
     position: absolute;
+    transform: scale(0.7);
     top: -3px;
-    left: 0px;
+    left: -3px;
   }
 
   .search-bar__tag-filter-box__reset-filter {

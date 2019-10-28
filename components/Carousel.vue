aaaf186f421ba6c4c5967b5702b860a58f08a1c7
Alexandre Mondt <a.mondt@didlt0018.lan>
2019-10-28 16:46:33 +0100

    add storyblok params / display images

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,12 +1,44 @@
 <template>
   <div>
     <h1>This is my carousel component</h1>
+
+    <div
+    style="display: flex; flex-wrap: wrap">
+      <img
+        v-for="image in blok.images"
+        :key="image.name"
+        v-lazy="lazyImage(image.filename)"
+        :alt="image.name"
+        width="50%"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import supportsWebP from 'supports-webp';
 
+export default {
+  props: {
+    blok: {
+      type: Object, default: function () {
+        return {}
+      }
+    }
+  },
+  mounted() {
+    console.log(this.blok.images)
+  },
+  methods: {
+    lazyImage(filename) {
+      /*
+        * If webp is supported by the client, serve a large webp image
+        * https://www.storyblok.com/docs/image-service
+        */
+      const filters = supportsWebP ? '/filters:format(webp)' : '';
+      return this.$options.filters.imageApi({src: filename, size: '', filters: filters});
+    },
+  }
 }
 </script>
 

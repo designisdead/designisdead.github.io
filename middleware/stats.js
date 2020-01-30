06fa5540dee7c37b650542a6d92350e8eac161b5
Kris Barnhoorn <kris@sumerion.com>
2020-01-30 19:13:09 +0100

    DIDW-118 formatting, inserting zulu dates and removing anonymousid

--- middleware/stats.js
+++ middleware/stats.js
@@ -1,14 +1,13 @@
 import axios from 'axios'
 
-export default function ({ route }) {
+export default function ({route}) {
+  let now = new Date().toISOString();
   return axios.post('https://webhook.site/cf0dfd39-a69a-41df-9640-dc5da9ae9c42', {
-   "anonymousId": "",
-   "type": "PageView",
-   "properties": {
-  	"page": "/more-testing",
-     "url": route.fullPath
-   },
-   "eventDate": "2019-10-09T14:45:45.100Z",
-   "createdAt": "2019-10-09T14:45:45.100Z"
+    "type": "PageView",
+    "properties": {
+      "page": route.fullPath
+    },
+    "eventDate": now,
+    "createdAt": now
   })
 }

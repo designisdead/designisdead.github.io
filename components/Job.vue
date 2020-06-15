2afa15ae269dccf8cfe90c4a6de80575fa8c1f34
Alexandre Mondt <alexandre.mondt@designisdead.com>
2020-06-15 11:28:13 +0200

    Job template refactor

--- components/Job.vue
+++ components/Job.vue
@@ -5,14 +5,21 @@ This component is used to render the Post content type on Storyblok
 <template>
   <main>
     <article>
-      <header class="Job-header Wrapper halfscreen u-textAlignCenter u-backgroundColor--black u-color--light">
-        <wrapper size="large">
-          <h1>{{ pagetitle }}</h1>
-          <h2 class="small">{{ blok.metadescription }}</h2>
-        </wrapper>
+      <wrapper 
+        size="large" 
+        :blok="{
+          backgroundimage: '//a.storyblok.com/f/44470/2928x1602/fb173b049c/groupe-2-copie-9-3x.png', 
+          backgroundposition: 'top right',
+          parallax: 0.5,
+          backgroundsize: 'contain'
+        }">
+        <header class="Job-header Wrapper halfscreen u-color--textLight">
+          <div class="Job__wrapper">
+            <h1 class="Job__title">{{ pagetitle }}</h1>
+            <h2 class="Job__subtitle">{{ blok.metadescription }}</h2>
+          </div>
       </header>
-
-      <spacer size="huge"/>
+    </wrapper>
 
       <div
         v-editable="blok"
@@ -66,5 +73,19 @@ This component is used to render the Post content type on Storyblok
 <style lang="scss">
   .Job-header {
     padding-bottom: $spacer;
+  }
+
+  .Job__wrapper {
+    width: 100%;
+  }
+
+  .Job__title {
+    text-transform: uppercase;
+    width: 40%;
+  }
+
+  .Job__subtitle {
+    font-size: 16px;
+    width: 40%;
   }
 </style>
